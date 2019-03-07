7de9dad63b80224e289f3ed22f96f7938ff80574
Vaibhav <vaibhavkhatod30@gmail.com>
2019-03-08 00:35:06 +0530

    Modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform,  ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 import { HomePage } from '../pages/home/home';
 import { AboutPage } from '../pages/about/about';
 import { MenuPage } from '../pages/menu/menu';
 import { ContactPage } from '../pages/contact/contact';
 import { FavoritesPage } from '../pages/favorites/favorites'
+import { ReservationPage } from '../pages/reservation/reservation';
 
 @Component({
   templateUrl: 'app.html'
@@ -19,7 +21,8 @@ export class MyApp {
 
   pages: Array<{title: string, icon: string, component: any}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,
+    public modalCtrl: ModalController) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -42,9 +45,16 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  openPage(page: { component: any; }) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  openReserve() {
+
+    let modal = this.modalCtrl.create(ReservationPage);
+    modal.present();
+  }
+
 }
